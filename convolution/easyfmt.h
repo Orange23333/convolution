021283415039474b05a5d1f5f39feb9a169fa653
Orange_233 <874462262@qq.com>
2021-12-21 17:40:19 +0800

    update LICENSE.

--- convolution/easyfmt.h
+++ convolution/easyfmt.h
@@ -11,9 +11,15 @@ class EasyFormat : QuestionFormat{
 private:
 	std::string 
 public:
+	//请务必从新的一行开始
 	void ReadFromText(const std::string text, const int beginLine){
 		int line = beginLine, row = 0;
-		char *ptr = text., ptr_end = text.
+		char *ptr = text.data(), ptr_end = text.data() + text.length();
+		
+#warning 可能存在地址达到最大值导致溢出的情况
+		for(;ptr!=ptr_end;ptr++){
+			
+		}
 	}
 	
 	EasyFormat(){
--- convolution/qstf.h
+++ convolution/qstf.h
@@ -3,12 +3,18 @@
 #ifndef __QSTF_H_
 	#define __QSTF_H_
 	
+	#include <string>
+	
 	#include "qstfmt.h"
 	#include "easyfmt.h"
 
 class QuestionFile{
 private:
 	QuestionFormat* questionFormat;
+public:
+	void LoadFromFile(std::string path){
+		
+	}
 }
 
 #endif
